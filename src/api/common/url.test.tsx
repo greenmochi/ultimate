a2cbb6293cbbb9bc9fbb0df2b6837115378fef8e
unknown <jasperjeng@gmail.com>
2019-06-04 06:16:23 -0700

    Check and test if endpoint and query params are valid

--- src/api/common/url.test.tsx
+++ src/api/common/url.test.tsx
@@ -48,3 +48,97 @@ test("invalid base url", () => {
   base = "http://localhost:123:456";
   expect(url.isBaseValid(base)).toBeFalsy();
 });
+
+test("valid endpoint", () => {
+  let endpoint: string = "/nyaa";
+  expect(url.isEndpointValid(endpoint)).toBeTruthy();
+  endpoint = "/nyaa/nyaa";
+  expect(url.isEndpointValid(endpoint)).toBeTruthy();
+  endpoint = "/nyaa/qerty"
+  expect(url.isEndpointValid(endpoint)).toBeTruthy();
+  endpoint = "/nyaa/qerty320487932jg%3215325235";
+  expect(url.isEndpointValid(endpoint)).toBeTruthy();
+  endpoint = "/nyaa/sdafio sadiof jsdoif";
+  expect(url.isEndpointValid(endpoint)).toBeTruthy();
+});
+
+test("invalid endpoint", () => {
+  let endpoint: string = "nyaa";
+  expect(url.isEndpointValid(endpoint)).toBeFalsy();
+  endpoint = "";
+  expect(url.isEndpointValid(endpoint)).toBeFalsy();
+});
+
+test("valid query parameter string", () => {
+  let queryParams: string = "?key1=value1";
+  expect(url.isQueryParamsValid(queryParams)).toBeTruthy();
+  queryParams = "?key1=value1&key2=value2";
+  expect(url.isQueryParamsValid(queryParams)).toBeTruthy();
+});
+
+test("invalid query parameter string", () => {
+  let queryParams: string = "key1=value1";
+  expect(url.isQueryParamsValid(queryParams)).toBeFalsy();
+  queryParams = "?";
+  expect(url.isQueryParamsValid(queryParams)).toBeFalsy();
+  queryParams = "";
+  expect(url.isQueryParamsValid(queryParams)).toBeFalsy();
+  queryParams = "?key1=value1&";
+  expect(url.isQueryParamsValid(queryParams)).toBeFalsy();
+  queryParams = "key1=value1&key2=value2";
+  expect(url.isQueryParamsValid(queryParams)).toBeFalsy();
+  queryParams = "?key1=value1key2=value2";
+  expect(url.isQueryParamsValid(queryParams)).toBeFalsy();
+  queryParams = "?key1=value1&key2=value2&";
+  expect(url.isQueryParamsValid(queryParams)).toBeFalsy();
+});
+
+test("valid url query string", () => {
+  let base: string = "http://localhost:8555/nyaa";
+  let endpoint: string = "/search";
+
+  let map: Map<string, string> = new Map<string, string>();
+  map.set("searchTerm", "boku no hero academia");
+  let queryParams: string = url.mapToQueryString(map);
+
+  expect(url.createQueryString(base, endpoint, queryParams)).toBe("http://localhost:8555/nyaa/search?searchTerm=boku no hero academia");
+
+  map.set("anotherKey", "anotherValue");
+  queryParams = url.mapToQueryString(map);
+
+  expect(url.createQueryString(base, endpoint, queryParams)).toBe("http://localhost:8555/nyaa/search?searchTerm=boku no hero academia&anotherKey=anotherValue");
+});
+
+test("invalid url query string", () => {
+  console.warn = jest.fn();
+
+  let base: string = "";
+  let endpoint: string = "/myendpoint";
+
+  let map: Map<string, string> = new Map<string, string>();
+  map.set("searchTerm", "boku no hero academia");
+  let queryParams: string = url.mapToQueryString(map);
+
+  expect(url.createQueryString(base, endpoint, queryParams)).toBe("");
+  expect(console.warn).toHaveBeenCalledTimes(1);
+
+  base = "http://localhost:8000";
+  endpoint = "";
+  queryParams = queryParams;
+
+  expect(url.createQueryString(base, endpoint, queryParams)).toBe("");
+  expect(console.warn).toHaveBeenCalledTimes(2);
+
+  base = "http://localhost:8000";
+  endpoint = "/myendpoint"
+  queryParams = "";
+
+  expect(url.createQueryString(base, endpoint, queryParams)).toBe("");
+  expect(console.warn).toHaveBeenCalledTimes(3);
+
+  base = "http://localhost:800033";
+  endpoint = "/myendpoint"
+  queryParams = url.mapToQueryString(map);
+  expect(url.createQueryString(base, endpoint, queryParams)).toBe("");
+  expect(console.warn).toHaveBeenCalledTimes(4);
+});
--- src/api/common/url.tsx
+++ src/api/common/url.tsx
@@ -1,7 +1,8 @@
 /**
- * Constructs a query parameter string
+ * Constructs a query parameter string.
  * @param map A map of key-value pairs
  * @param prefix Prefix to to the query string, defaults to "?"
+ * @returns A query parameter string
  */
 export function mapToQueryString(map: Map<string, string>, prefix: string = "?"): string {
   if (map.size <= 0) {
@@ -16,12 +17,18 @@ export function mapToQueryString(map: Map<string, string>, prefix: string = "?")
   return prefix + keyValuePairs.join("&");
 }
 
+/**
+ * Checks if the base url is in a valid localhost url.
+ * 
+ * e.g http://localhost:8555
+ * @param base Proxy server base url (localhost)
+ * @returns Return true if `base` is not malformed
+ */
 export function isBaseValid(base: string): boolean {
   const regexp = new RegExp(/https?:\/\/localhost:[0-9]+/);
   if (!regexp.test(base)) {
     return false;
   }
-  // const portRegexp = new RegExp(/[0-9]+/);
   const match: RegExpMatchArray | null = base.match(/[0-9]+/g);
   if (!match || match.length != 1) {
     return false;
@@ -34,20 +41,59 @@ export function isBaseValid(base: string): boolean {
   return port >= 0 && port <= 65535;
 }
 
+/**
+ * Checks if the endpoint is formed correctly.
+ * 
+ * e.g /myendpoint
+ * @param endpoint Proxy/gRPC endpoint
+ * @returns Return true if `endpoint` is not malformed
+ */
+export function isEndpointValid(endpoint: string): boolean {
+  const match: RegExpMatchArray | null = endpoint.match(/\/.*/g);
+  if (!match || match.length != 1) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Checks if the query parameters are formed correctly.
+ * 
+ * e.g ?key1=value1&key2=value2
+ * @param queryParams Query parameters
+ * @returns Return true if `queryParams` is not malformed
+ */
+export function isQueryParamsValid(queryParams: string): boolean {
+  const match: RegExpMatchArray | null = queryParams.match(/^\?([^=&]+=[^=&]+\&)*([^=&]+=[^=&]+)$/g);
+  if (!match || match.length != 1) {
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Creates a well-formed url with query parameters.
+ * 
+ * Example: http://localhost:8555/nyaa/search?sort=true&searchTerm=boku no hero academia
+ * @param base Proxy server base url (localhost)
+ * @param endpoint Proxy/gRPC endpoint
+ * @param queryParams Query parameters
+ * @returns Formatted url string with query parameters
+ */
 export function createQueryString(base: string, endpoint: string, queryParams: string): string {
-  if (!base) {
+  if (!base || !isBaseValid(base)) {
     console.warn(`Problem creating query string: base=${base}`);
     return "";
   } 
   
-  if (!endpoint) {
+  if (!endpoint || !isEndpointValid(endpoint)) {
     console.warn(`Problem creating query string: endpoint=${endpoint}`);
     return "";
   }
 
-  if (!queryParams) {
+  if (!queryParams || !isQueryParamsValid(queryParams)) {
     console.warn(`Problem creating query string: queryParams=${queryParams}`);
     return "";
   }
-  return "";
+  return base + endpoint + queryParams;
 }
