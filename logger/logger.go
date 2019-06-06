51fcd78e4f77f54240a6c8f8ba57a81db26271ef
unknown <jasperjeng@gmail.com>
2019-06-06 05:23:13 -0700

    Add info, warning, error, fatal log leve

---
+++ logger/logger.go
@@ -0,0 +1,91 @@
+package logger
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var name = "kabedon-nyaa.log"
+
+var file *os.File
+
+var logger = newLogger()
+
+func newLogger() *log.Logger {
+	f, err := os.Create(name)
+	if err != nil {
+		log.Println("unable to create log file", name)
+		return log.New(os.Stderr, "", log.Ldate|log.Ltime|log.Lshortfile)
+	}
+	file = f
+	return log.New(file, "", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+// Close close file
+func Close() {
+	file.Close()
+}
+
+// Info writes message
+func Info(args ...interface{}) {
+	logger.Print(args...)
+}
+
+// Infoln writes messsage with new line
+func Infoln(args ...interface{}) {
+	Info(fmt.Sprintln(args...))
+}
+
+// Infof writes formatted message
+func Infof(format string, args ...interface{}) {
+	Info(fmt.Sprintf(format, args...))
+}
+
+// Warning writes warning message
+func Warning(args ...interface{}) {
+	logger.SetPrefix("[WARNING] ")
+	Info(args...)
+}
+
+// Warningln writes warning message with new line
+func Warningln(args ...interface{}) {
+	Warning(fmt.Sprintln(args...))
+}
+
+// Warningf writes formatted warning message
+func Warningf(format string, args ...interface{}) {
+	Warning(fmt.Sprintf(format, args...))
+}
+
+// Error writes error message
+func Error(args ...interface{}) {
+	logger.SetPrefix("[ERROR] ")
+	Info(args...)
+}
+
+// Errorln writes error message with new line
+func Errorln(args ...interface{}) {
+	Error(fmt.Sprintln(args...))
+}
+
+// Errorf writes formatted error message
+func Errorf(format string, args ...interface{}) {
+	Error(fmt.Sprintf(format, args...))
+}
+
+// Fatal writes fatal message
+func Fatal(args ...interface{}) {
+	logger.SetPrefix("[FATAL] ")
+	Info(args...)
+}
+
+// Fataln writes fatal message with new line
+func Fataln(args ...interface{}) {
+	Fatal(fmt.Sprintln(args...))
+}
+
+// Fatalf writes formatted fatal message
+func Fatalf(format string, args ...interface{}) {
+	Fatal(fmt.Sprintf(format, args...))
+}
--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"os"
-	"fmt"
-	"log"
 	"github.com/greenmochi/kabedon-nyaa/torrent"
+	"github.com/greenmochi/kabedon-nyaa/logger"
 )
 
 const (
@@ -18,23 +17,20 @@ func main() {
 	// anacrolix/torrent is outputting crazy noise, turn it off and log to file instead
 	os.Stdout = nil
 
-	f, err := os.Create("kabedon-nyaa.log")
-	if err != nil {
-		log.Println("unable to create log file")
-	}
-	defer f.Close()
+	defer logger.Close()
 
 	client, err := torrent.NewClient()
 	if err != nil {
-		fmt.Fprintf(f, "%+v\n", err)
+		logger.Fatalf("%+v\n", err)
 		os.Exit(1)
 	}
 	defer client.Close()
 
-	fmt.Fprintf(f, "Adding magnet")
+	logger.Infoln("Adding magnet")
 	magnet := "magnet:?xt=urn:btih:464aa94a230061049a1017c7550e6cf724aa9767&dn=%5BHorribleSubs%5D%20Sewayaki%20Kitsune%20no%20Senko-san%20-%2009%20%5B360p%5D.mkv&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce"
 	if ok := client.AddMagnet(magnet); !ok {
-		fmt.Fprintf(f, "unable to add magnet")
+		// fmt.Fprintf(f, "unable to add magnet")
+		logger.Error("Unable to add magnet")
 	}
-	fmt.Fprintf(f, "done")
+	logger.Infoln("Done")
 }
