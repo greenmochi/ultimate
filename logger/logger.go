d0345b4d28973d937e38d20109d40151e88b1077
unknown <jasperjeng@gmail.com>
2019-06-12 02:58:05 -0700

    Print the proper file name by setting calldepth in output

--- logger/logger.go
+++ logger/logger.go
@@ -30,63 +30,71 @@ func Close() {
 // Info writes message
 func Info(args ...interface{}) {
 	logger.SetPrefix("[INFO] ")
-	logger.Print(args...)
+	logger.Output(2, fmt.Sprint(args...))
 }
 
 // Infoln writes messsage with new line
 func Infoln(args ...interface{}) {
-	Info(fmt.Sprintln(args...))
+	logger.SetPrefix("[INFO] ")
+	logger.Output(2, fmt.Sprintln(args...))
 }
 
 // Infof writes formatted message
 func Infof(format string, args ...interface{}) {
-	Info(fmt.Sprintf(format, args...))
+	logger.SetPrefix("[INFO] ")
+	logger.Output(2, fmt.Sprintf(format, args...))
 }
 
 // Warning writes warning message
 func Warning(args ...interface{}) {
 	logger.SetPrefix("[WARNING] ")
-	Info(args...)
+	logger.Output(2, fmt.Sprint(args...))
 }
 
 // Warningln writes warning message with new line
 func Warningln(args ...interface{}) {
-	Warning(fmt.Sprintln(args...))
+	logger.SetPrefix("[WARNING] ")
+	logger.Output(2, fmt.Sprintln(args...))
 }
 
 // Warningf writes formatted warning message
 func Warningf(format string, args ...interface{}) {
-	Warning(fmt.Sprintf(format, args...))
+	logger.SetPrefix("[WARNING] ")
+	logger.Output(2, fmt.Sprintf(format, args...))
 }
 
 // Error writes error message
 func Error(args ...interface{}) {
 	logger.SetPrefix("[ERROR] ")
-	Info(args...)
+	logger.Output(2, fmt.Sprint(args...))
 }
 
 // Errorln writes error message with new line
 func Errorln(args ...interface{}) {
-	Error(fmt.Sprintln(args...))
+	logger.SetPrefix("[ERROR] ")
+	logger.Output(2, fmt.Sprintln(args...))
 }
 
 // Errorf writes formatted error message
 func Errorf(format string, args ...interface{}) {
-	Error(fmt.Sprintf(format, args...))
+	logger.SetPrefix("[ERROR] ")
+	logger.Output(2, fmt.Sprintf(format, args...))
 }
 
 // Fatal writes fatal message
 func Fatal(args ...interface{}) {
 	logger.SetPrefix("[FATAL] ")
-	Info(args...)
+	logger.Output(2, fmt.Sprint(args...))
 }
 
 // Fatalln writes fatal message with new line
 func Fatalln(args ...interface{}) {
-	Fatal(fmt.Sprintln(args...))
+	logger.SetPrefix("[FATAL] ")
+	logger.Output(2, fmt.Sprintln(args...))
 }
 
 // Fatalf writes formatted fatal message
 func Fatalf(format string, args ...interface{}) {
-	Fatal(fmt.Sprintf(format, args...))
+	logger.SetPrefix("[FATAL] ")
+	logger.Output(2, fmt.Sprintf(format, args...))
 }
