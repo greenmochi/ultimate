c1532f9adf4624c754591d891bb460ee22939347
unknown <jasperjeng@gmail.com>
2019-07-07 18:52:32 -0700

    Save work before computer crashes again

--- ultimate_torrent/service/ultimate_torrent_service.py
+++ ultimate_torrent/service/ultimate_torrent_service.py
@@ -1,6 +1,7 @@
+import time
+
 from ultimate_torrent.service import ultimate_torrent_pb2
 from ultimate_torrent.service import ultimate_torrent_pb2_grpc
-
 
 class UltimateTorrentService(ultimate_torrent_pb2_grpc.UltimateTorrentServicer):
     """Implements UltimateTorrentServier functionalities"""
@@ -15,3 +16,10 @@ class UltimateTorrentService(ultimate_torrent_pb2_grpc.UltimateTorrentServicer):
     def Shutdown(self, request, context):
         msg = "Got your message: " + request.message
         return ultimate_torrent_pb2.ShutdownReply(message=msg)
+
+    def AllTorrentStatus(self, request, context):
+        msg = "Go your AllTorrentStatus request: " + request.message
+        for i in range(3):
+            msg = msg + " " + str(i)
+            time.sleep(1)
+            yield ultimate_torrent_pb2.AllTorrentStatusReply(message=msg)
