1c8521445862ea3354b9b703d45ac07ab6f85834
unknown <jasperjeng@gmail.com>
2019-07-28 02:05:16 -0700

    Create user anime and user anime list tables

--- services/myanimelist/myanimelist/database/database.go
+++ services/myanimelist/myanimelist/database/database.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 
 	_ "github.com/mattn/go-sqlite3"
+	log "github.com/sirupsen/logrus"
 )
 
 type Database struct {
@@ -22,9 +23,59 @@ func (d *Database) Open(name string) error {
 		return err
 	}
 	d.db = db
+	d.createTables()
 	return nil
 }
 
 func (d *Database) Close() {
 	d.db.Close()
+}
+
+func (d *Database) createTables() {
+	stmt := `
+	CREATE TABLE IF NOT EXISTS user_anime_list (
+		username STRING NOT NULL PRIMARY KEY
+	);
+	CREATE TABLE IF NOT EXISTS user_anime (
+		username STRING NOT NULL,
+		status INTEGER,
+		score INTEGER,
+		tags TEXT,
+		is_rewatching INTEGER,
+		num_watched_episodes INTEGER,
+		anime_title TEXT,
+		anime_num_episodes INTEGER,
+		anime_airing_status INTEGER,
+		anime_id INTEGER,
+		anime_studios TEXT,
+		anime_licensors TEXT,
+
+		anime_season_year INTEGER,
+		anime_season_season TEXT,
+
+		has_episode_video INTEGER,
+		has_promotion_video INTEGER,
+		has_video INTEGER,
+		video_url TEXT,
+		anime_url TEXT,
+		anime_image_path TEXT,
+		is_added_to_list INTEGER,
+		anime_media_type_string TEXT,
+		anime_mpaa_rating_string TEXT,
+		start_date_string TEXT,
+		finish_date_string TEXT,
+		anime_start_date_string TEXT,
+		anime_end_date_string TEXT,
+		days_string INTEGER,
+		storage_string TEXT,
+		priority_string TEXT,
+
+		FOREIGN KEY (username) REFERENCES user_anime_list (username) ON DELETE CASCADE ON UPDATE CASCADE
+	);
+	`
+	_, err := d.db.Exec(stmt)
+	if err != nil {
+		log.Error(err)
+		return
+	}
 }
