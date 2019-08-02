4d783cead0f62dbebcf82e21fda017b66cd3717c
unknown <jasperjeng@gmail.com>
2019-08-02 00:58:40 -0700

    Remove some redundant logging

--- services/myanimelist/myanimelist/myanimelist.go
+++ services/myanimelist/myanimelist/myanimelist.go
@@ -27,6 +27,7 @@ func New() *MyAnimeList {
 func (mal *MyAnimeList) InitDB(path string) {
 	if err := mal.db.Open(path); err != nil {
 		log.Error(err)
+		return
 	}
 	log.Infof("Successfully initialized database: %s", path)
 }
@@ -40,7 +41,7 @@ func (mal *MyAnimeList) CloseDB() {
 func (mal *MyAnimeList) storeUserAnimeList(user string, userAnimeList *data.UserAnimeList) error {
 	mal.store.SetUserAnimeList(user, userAnimeList)
 	if err := mal.db.InsertUserAnimeList(userAnimeList); err != nil {
-		log.Error(err)
+		log.Errorf("Failed to insert %s anime list into database: %s", user, err)
 		return err
 	}
 	log.Infof("Successfully saved user %s anime list in the database", user)
@@ -48,16 +49,15 @@ func (mal *MyAnimeList) storeUserAnimeList(user string, userAnimeList *data.User
 }
 
 // GetUserAnimeList TODO
-func (mal *MyAnimeList) GetUserAnimeList(user string) error {
-	// Fetch user anime list (and stores it)
+func (mal *MyAnimeList) GetUserAnimeList(user string) (*data.UserAnimeList, error) {
 	userAnimeList, err := fetch.UserAnimeList(user)
 	if err != nil {
-		log.Error(err)
+		return nil, err
 	}
 	if err := mal.storeUserAnimeList(user, userAnimeList); err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+	return userAnimeList, nil
 }
 
 func (mal *MyAnimeList) storeAnimeSearchResults(results []*data.AnimeSearchResult) {
@@ -69,7 +69,6 @@ func (mal *MyAnimeList) storeAnimeSearchResults(results []*data.AnimeSearchResul
 func (mal *MyAnimeList) SearchAnime(query string) ([]*data.AnimeSearchResult, error) {
 	results, err := fetch.AnimeSearchResults(query)
 	if err != nil {
-		log.Error(err)
 		return nil, err
 	}
 	mal.storeAnimeSearchResults(results)
@@ -80,6 +79,7 @@ func (mal *MyAnimeList) storeAnime(anime *data.Anime) error {
 	mal.store.SetAnime(anime.ID, anime)
 	if err := mal.db.InsertAnime(anime); err != nil {
 		log.Errorf("Failed to insert anime %s into database: %s", anime.Title, err)
+		return err
 	}
 	log.Infof("Successfully saved anime %s to the database", anime.Title)
 	return nil
@@ -89,11 +89,9 @@ func (mal *MyAnimeList) storeAnime(anime *data.Anime) error {
 func (mal *MyAnimeList) GetAnimeBySearchResult(result *data.AnimeSearchResult) (*data.Anime, error) {
 	anime, err := fetch.AnimeBySearchResult(result)
 	if err != nil {
-		log.Error(err)
 		return nil, err
 	}
 	if err := mal.storeAnime(anime); err != nil {
-		log.Errorf("Failed to save anime: %s", err)
 		return anime, nil
 	}
 	return anime, nil
