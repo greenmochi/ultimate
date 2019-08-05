d7fe685730463919b607451b7bc5a37c8c095c48
unknown <jasperjeng@gmail.com>
2019-08-05 01:03:50 -0700

    Hook rpc GetAnimeByID with gateway

--- services/gateway/proto/myanimelist/myanimelist.pb.gw.go
+++ services/gateway/proto/myanimelist/myanimelist.pb.gw.go
@@ -97,6 +97,23 @@ func request_MyAnimeList_GetAnimeByLink_0(ctx context.Context, marshaler runtime
 
 }
 
+func request_MyAnimeList_GetAnimeByID_0(ctx context.Context, marshaler runtime.Marshaler, client MyAnimeListClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq message.AnimeID
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetAnimeByID(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterMyAnimeListHandlerFromEndpoint is same as RegisterMyAnimeListHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterMyAnimeListHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -215,6 +232,26 @@ func RegisterMyAnimeListHandlerClient(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
+	mux.Handle("POST", pattern_MyAnimeList_GetAnimeByID_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_MyAnimeList_GetAnimeByID_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_MyAnimeList_GetAnimeByID_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -226,6 +263,8 @@ var (
 	pattern_MyAnimeList_SearchAnime_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"myanimelist", "SearchAnime"}, ""))
 
 	pattern_MyAnimeList_GetAnimeByLink_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"myanimelist", "GetAnimeByLink"}, ""))
+
+	pattern_MyAnimeList_GetAnimeByID_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"myanimelist", "GetAnimeByID"}, ""))
 )
 
 var (
@@ -236,4 +275,6 @@ var (
 	forward_MyAnimeList_SearchAnime_0 = runtime.ForwardResponseMessage
 
 	forward_MyAnimeList_GetAnimeByLink_0 = runtime.ForwardResponseMessage
+
+	forward_MyAnimeList_GetAnimeByID_0 = runtime.ForwardResponseMessage
 )
