b4b615137548d8139491917bad80dca6f5c189f4
unknown <jasperjeng@gmail.com>
2019-08-06 23:36:04 -0700

    Change to download manager

---
+++ services/youtube-dl/youtube_dl/download_manager.py
@@ -0,0 +1,12 @@
+class DownloadManager():
+    """Downloads youtube videos from a queue"""
+
+    def __init__(self):
+        self.queue = []
+    
+    def add_to_queue(self, url, opts):
+        pass
+
+    def download(self):
+
+    
