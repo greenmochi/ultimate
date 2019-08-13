d80e2c159ba33e3bd2e9334a4def62fd0dff576f
unknown <jasperjeng@gmail.com>
2019-08-12 23:28:00 -0700

    Add atlas and youtubedl to gulp start task

--- scripts/gulp.start.js
+++ scripts/gulp.start.js
@@ -67,10 +67,38 @@ function startMyAnimeList() {
   return script;
 }
 
+function startYoutubeDL() {
+  const cmd = `"services/youtube-dl/dist/youtube-dl/youtube-dl"`;
+  const script = exec(cmd, {
+    cwd: path.resolve(process.cwd()),
+  });
+  script.stdout.pipe(process.stdout);
+  script.stderr.pipe(process.stderr);
+  script.stderr.on("error", (error) => {
+    done(error);
+  });
+  return script;
+}
+
+function startAtlas() {
+  const cmd = `"services/atlas/build/atlas"`;
+  const script = exec(cmd, {
+    cwd: path.resolve(process.cwd()),
+  });
+  script.stdout.pipe(process.stdout);
+  script.stderr.pipe(process.stderr);
+  script.stderr.on("error", (error) => {
+    done(error);
+  });
+  return script;
+}
+
 module.exports = {
   startUI,
   startElectron,
   startGateway,
   startNyaa,
   startMyAnimeList,
+  startYoutubeDL,
+  startAtlas,
 };
